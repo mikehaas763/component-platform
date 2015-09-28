3e1305df2d9e7dae25e301fb1018ada2a89026fd
Mike Haas <mike@emken.co>
2015-09-27 22:49:02 -0400

    Get initial app component rendered

---
+++ ComponentDashboard/App.ts
@@ -0,0 +1,17 @@
+///<reference path="../typings/angular2/angular2.d.ts"/>
+
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component({
+    selector: 'my-app'
+})
+@View({
+    template: '<h1 id="output">My First Angular 2 App</h1>'
+})
+export default class App {
+
+}
+
+bootstrap(App);
+
+console.log('executed');
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,31 @@
 var gulp = require('gulp');
 var gulpTypeScript = require('gulp-typescript');
 var del = require('del');
+var gulpFilter = require('gulp-filter');
 
-gulp.task('default', ['clean'], () => {
-    return gulp.src(['ComponentDashboard/**/*' + '.ts'], { base: './' })
-        .pipe(gulpTypeScript({
-            module: 'system'
-        }))
-        .pipe(gulp.dest('out'));
-});
+var config = {
+    buildOutput: 'out',
+    sourceTree: ['ComponentDashboard/**/*', 'index.html']
+};
+
+gulp.task('default', ['clean'], buildFactory(config));
+
+function buildFactory(config) {
+    return () => {
+        var tsFilter = gulpFilter(['**/*.ts'], { restore: true });
+
+        return gulp.src(config.sourceTree, { base: './' })
+            .pipe(tsFilter)
+            .pipe(gulpTypeScript({
+                module: 'system',
+                emitDecoratorMetadata: true,
+                "experimentalDecorators": true,
+                target: 'ES5'
+            }))
+            .pipe(tsFilter.restore)
+            .pipe(gulp.dest(config.buildOutput));
+    };
+}
 
 gulp.task('clean', () => {
     return del('out');
